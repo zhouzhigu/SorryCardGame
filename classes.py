74ebe375f3c526bb43f1fd6a32eff6e3ed7546f6
Alex Sederquest <mrbiggkahuna12@yahoo.com>
2015-04-20 14:31:39 -0400

    Updating more of the classes

--- classes.py
+++ classes.py
@@ -6,11 +6,19 @@ class Hand(list):
 		self.append(card)
 		
 	def discard(self, card):
-		self.remove(card)
+		self.pop(card)
+		
+class Player(Hand):
+	
+	def __init__(self):
+		self.decks = Decks()
+		
+	def discard(self, card):
+		decks.discard.self.Hand.pop(card)
 		
 class Cards(object):
 
-	def __init__(self, card_count, sorry_count, card_deck, sorry_deck):
+	def __init__(self, card_count, sorry_count, card_deck, sorry_deck, sorry_discard_deck, card_discard_deck):
 		self.card_count = 41
 		self.sorry_count = 12
 		self.card_deck = ['1', '1', '1', '1', '2: Go again', '2: Go again', '2: Go again', '2: Go again', '3: Take a card from an opponents hand',
@@ -25,22 +33,32 @@ class Cards(object):
 							   'Everyone gives you a card from their hand', 'Go home! Any one set of yours is now complete', 'Discard all 12\'s from all sets but yours', 'Look at another player\'s hand, if you are able, take one of those cards and play it',
 							   'Look at another player\'s hand, if you are able, take one of those cards and play it', 'Draw 2 cards, if you are able play 1 of them and discard the other. Otherwise discard both', 'Draw 2 cards, if you are able play 1 of them and discard the other. Otherwise discard both',
 							   'If you are able, take the last card from another player\'s set and add it to one of yours', 'If you are able, take the last card from another player\'s set and add it to one of yours']
+		self.sorry_discard_deck = []
+		self.card_discard_deck = []
 		shuffle(self.card_deck)
 		shuffle(self.sorry_deck)
 	
-class Decks(Cards):
+class CardDeck(Cards):
 	
 	def cardReshuffle(self):
 		pass
-		
-	def sorryReshuffle(self):
-		pass
-		
-	def cardDiscard(self):
-		pass
-		
-	def sorryDiscard(self):
-		pass
+
+	def cardDiscard(self, card):
+		self.card_discard_deck.append(card)
 		
 	def deal(self):
+		pass
+		
+	def draw(self):
+		pass
+		
+class SorryDeck(Cards):
+	
+	def reshuffle(self):
+		pass
+	
+	def discard(self):
+		pass
+		
+	def draw(self):
 		pass
