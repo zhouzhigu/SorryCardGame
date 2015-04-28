e761ad8138f6cb5dcaa6e5b2420d8d396de63833
techwcs <bsoistmann@wilmingtonchristian.org>
2015-04-28 14:13:23 -0400

    Update classes.py

--- classes.py
+++ classes.py
@@ -12,53 +12,49 @@ class Set(list, Hand):
 
 	def add(self, card):
 		pass
-		
+
 	def isComplete(self):
 		pass
-		
+
 	def getCardKey(self, card):
 		pass
-		
+
 	def removeCardFromSet(self, card):
 		pass
-		
+
 class Hand(list, Player):
 
 	def add(self, card):
 		pass
-		
+
 	def discard(self, cardIndex):
 		pass
-	
+
 	def getCardIndex(self, card):
 		pass
-		
+
 	def playCard(self):
 		pass
-		
+
 class Player(object):
 
 	def __init__(self):
 		pass
-		
+
 	def addPlayer(self, name, age):
 		pass
-		
+
 	def orderToPlay(self):
 		pass
-		
-class Cards(object):
+
+class Deck(object):
 
 	def __init__(self):
 		pass
-		
-class Decks(cards):
 
-	def __init__(self):
-		pass
-		
 	def playingCardDraw(self):
 		pass
-		
+
 	def sorryCardDraw(self):
 		pass
+
