b2ae2b5d8b9eb3573e772689621d48a802de3993
Brendan Tomazine <tomazine.brendan@wcsstudents.com>
2015-05-07 05:25:33 -0400

    instructions

--- sorry.py
+++ sorry.py
@@ -7,15 +7,38 @@ def printIntroduction():
 # Brendan
 def printInstructions():
     # print instructions
-    pass
+    print """
+Object: Be the first person to flip over all your Start cards
+Setup: Pick a blue, green, red or yellow card as your Start card and have them face down; 
+depending on the number of players. (4 players max) 
+Now, shuffle the playing cards and give four to each person playing
+Then, shuffle the sorry cards and playing cards, and put them into two seperate piles.
+Once that is done, you are ready to start the game. Youngest player goes first."""
+
+""" 
+raw_input("press Enter")
+
+print """
+On your turn: you must do two things:
+1. draw a card
+2. play a card, and discard it
+Drawing a card: you must draw a card from the playing deck and put it in your hand. 
+Then you may proceed to play a card. If the card has a number on it, you may put it ontop 
+of your Start card. Your Start card must reach 15 before you can flip it. It cannot be over 15,
+it must be 15. 
+
+"""
+
 
 # Brendan
 def getNumberOfPlayers():
-    pass
+    print 
+	"How many players?"
 
 # Brendan
 def getPlayerAge():
-    pass
+    print 
+	"How old is the player"
 
 sorryGame = SorryGame()
 
